babe3a72febd71150f5da5d51f990ee6385e73a8
Umran <mohammedumran.0416@gmail.com>
2025-09-29 21:34:14 +0100

    creating random number fucntion

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,25 @@
 export default function Die(props){
+    
+
+    
+    function generateNewDice(){
+        let diceArray = [ ]
+        for (let i = 0; i < 10; i++){
+            diceArray.push(Math.floor(Math.random() * 6) + 1)
+        }
+        
+      return diceArray
+    }
+
+    console.log(generateNewDice())
+
+    
+
+   
     return(
         <>
         
-        <button className="die">{props.value}</button>
+            <button onClick={generateNewDice} className="die">{props.value}</button>
         
         </>
     )
