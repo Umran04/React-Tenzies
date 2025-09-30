894205af2edd477bf6eec4bca509a9e66f5c307d
Umran <mohammedumran.0416@gmail.com>
2025-09-30 15:43:23 +0100

    adding feature to check if both conditions to win the game has been met, if so log out game over

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,23 +20,28 @@ export default function App(){
       return diceArray
     }
 
-    const [diceRoll,setDiceRoll] = useState(generateNewDice())
+    const [dice,setDice] = useState(generateNewDice())
 
 
     function reRoll(){
-        setDiceRoll(prevDice => prevDice.map(die => {
+        setDice(prevDice => prevDice.map(die => {
             return die.isHeld === false ? {...die, value: Math.floor(Math.random() * 6) + 1} : die
         }))
     }
 
     function hold(id){
         
-        setDiceRoll(prevDice => prevDice.map(die => {
+        setDice(prevDice => prevDice.map(die => {
             return die.id === id? {...die, isHeld : !die.isHeld} : die
         }))
     }
 
-    let roll = diceRoll.map(diceObj =>
+    if(dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)){
+        console.log('Game Won')
+    }
+    
+
+    let roll = dice.map(diceObj =>
         <Die 
         key={diceObj.id}
         id={diceObj.id} 
